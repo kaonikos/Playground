c4c473e5ef7b9aefff9911dceb93fe81016d3ece
Nick Vasilop <nicolas.vasilop@gmail.com>
2022-04-07 23:42:04 +0300

    made moving blob

--- src/components/Body/index.jsx
+++ src/components/Body/index.jsx
@@ -1,17 +1,44 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
+import {createUseStyles} from 'react-jss'
 import './styles.css'
 
 const Body = () => {
 
+    const [top, setTop] = useState()
+    const [left, setLeft] = useState()
+    const [move, setMove] = useState(false)
+
+    useEffect(
+        () => {
+            const element = document.getElementById('blob')
+            if (element) {
+                element.style.position = "absolute";
+                element.style.top = `${top}px`
+                element.style.left = `${left}px`
+            }
+            console.log(top,left)
+        }, [top,left]
+    )
+
+    useEffect(
+        () => {
+            if (move) {
+                onmousemove = (e) => {
+                    setTop(e.clientY)
+                    setLeft(e.clientX)
+                }
+            } else {
+                onmousemove = (e) => {
+
+                }
+            }
+        },[move]
+    )
+
     return (
         <div className='body'>
-            <div className='blob'>
+            <div className='blob' id='blob' onClick={() => setMove(!move)}>
             </div>
-            <div className='blob'>
-            </div>
-            <div className='blob'>
-            </div>
-
         </div>
     )
 }
